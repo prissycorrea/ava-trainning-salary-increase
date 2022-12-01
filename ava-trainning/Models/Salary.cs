ee613a086de81c4933eab8d1fb7e8ae519649188
Priscilla Correa <99624784+prissycorrea@users.noreply.github.com>
2022-12-01 11:39:12 -0300

    Add files via upload

---
+++ ava-trainning/Models/Salary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ava_trainning.Models
+{
+    internal class Salary
+    {
+    }
+}
---
+++ ava-trainning/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace Avanade.Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 0;
+            double salaryIncrease = 0;
+            double increase = 0.10;
+            string employee;
+
+            Console.WriteLine("Digite o nome do funcionário: ");
+            employee = Console.ReadLine();
+
+            Console.WriteLine("Entre com o salário do funcionário: R$ ");
+            salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"O salário inicial do(a) funcionário(a) {employee} é R$ {salary.ToString("F2", CultureInfo.CreateSpecificCulture("pt-BR"))}");
+            salaryIncrease = salary + (salary * increase);
+
+            Console.WriteLine($"Com o aumento de {increase * 100}% o salário passa a ser R$ {salaryIncrease.ToString("F2", CultureInfo.CreateSpecificCulture("pt-BR"))}");
+            Console.WriteLine($"O valor do aumento foi R$ {(salaryIncrease - salary).ToString("F2", CultureInfo.CreateSpecificCulture("pt-BR"))}");
+            Console.ReadLine();
+        }
+    }
+}
